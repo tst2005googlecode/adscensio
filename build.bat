b7bfb4813196efdcf802089bfc38ca44dc0e75cf
Joel <Joel@localhost>
2012-10-02 19:57:15 +0430

    Moved images into new folder Began moving map code into levels.lua Removed game.love

--- build.bat
+++ build.bat
@@ -1,4 +1,7 @@
+-- This batch script makes it easier to build the game
 PATH=%~dp0
 cd %PATH%Source
+-- If you do not have 7-Zip then change the command for your preference
+-- or just get it at www.7-zip.org
 "C:\Program Files\7-Zip\7z.exe" a -tzip "%PATH%game.love" *
 PAUSE
