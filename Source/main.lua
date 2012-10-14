69d6e78773a8c31ea591b934d3d7a8b0566c5b3a
Joel <Joel@localhost>
2012-10-14 07:57:00 +0430

    Added joystick controls

--- Source/main.lua
+++ Source/main.lua
@@ -10,6 +10,11 @@ for i=0,3 do -- change 3 to the number of tile images minus 1.
   tile[i] = love.graphics.newImage( "images/tile"..i..".png" )
 end
 
+joysticks = love.joystick.getNumJoysticks()
+love.joystick.open(1)
+button = love.joystick.getNumButtons(1)
+axes = love.joystick.getNumAxes(1)
+
 game_start = false
 Menu.load()
 
@@ -31,6 +36,10 @@ function draw_character_position()
 	love.graphics.print(hero.x + Levels.get_map_x_pos(), 40, 35)
 	love.graphics.print(hero.y + Levels.get_map_y_pos(), 40, 55)
 	love.graphics.print(Menu.active_panel, 40, 75)
+	love.graphics.print(joysticks, 40, 95)
+	love.graphics.print(button, 40, 115)
+	love.graphics.print(axes, 40, 135)
+	love.graphics.print("axis: "..love.joystick.getHat(1, 1), 40, 155)
 end
 
 function get_character_x_pos()
@@ -78,54 +87,62 @@ function love.update(dt)
 		Menu.update_main(dt)
 	elseif game_start == true and Menu.active_panel == "none" then
 		-- get input for directional movement
-		if love.keyboard.isDown("down") and get_character_y_pos() < (Levels.map_h - 1) * 40 then
-			hero.dir = "down"
-			if hero.y < 400 then
-			   hero.y = hero.y + hero.dt * dt
-			else
-			   Levels.changeOffset("down", dt * hero.dt)
-			   Entities.y = Entities.y - hero.dt * dt
-			end
-			if Entities.check_collision(hero.x, hero.y) then
-				resolve_collision(hero.dir, Entities.x, Entities.y)
-			end
-		end
-		if love.keyboard.isDown("up") and get_character_y_pos() > 0 then
-			hero.dir = "up"
-			if hero.y > 150 then
-			   hero.y = hero.y - hero.dt * dt
-			else
-			   Levels.changeOffset("up", dt * hero.dt)
-			   Entities.y = Entities.y + hero.dt * dt
-			end
-			if Entities.check_collision(hero.x, hero.y) then
-				resolve_collision(hero.dir, Entities.x, Entities.y)
+		if love.keyboard.isDown("down") or love.joystick.getHat(1, 1) == "d" or love.joystick.getHat(1, 1) == "ld" or love.joystick.getHat(1, 1) == "rd" then 
+			if get_character_y_pos() < (Levels.map_h - 1) * 40 then
+				hero.dir = "down"
+				if hero.y < 400 then
+				   hero.y = hero.y + hero.dt * dt
+				else
+				   Levels.changeOffset("down", dt * hero.dt)
+				   Entities.y = Entities.y - hero.dt * dt
+				end
+				if Entities.check_collision(hero.x, hero.y) then
+					resolve_collision(hero.dir, Entities.x, Entities.y)
+				end
 			end
 		end
-		if love.keyboard.isDown("right") and get_character_x_pos() < (Levels.map_w - 1) * 40 then
-			hero.dir = "right"
-			if hero.x < 600 then
-			   hero.x = hero.x + hero.dt * dt
-			else
-			   Levels.changeOffset("right", dt * hero.dt)
-			   Entities.x = Entities.x - hero.dt * dt
-			end
-			if Entities.check_collision(hero.x, hero.y) then
-				--hero.x = hero.x - hero.dt * dt
-				resolve_collision(hero.dir, Entities.x, Entities.y)
+		if love.keyboard.isDown("up") or love.joystick.getHat(1, 1) == "u" or love.joystick.getHat(1, 1) == "lu" or love.joystick.getHat(1, 1) == "ru" then 
+			if get_character_y_pos() > 0 then
+				hero.dir = "up"
+				if hero.y > 150 then
+				   hero.y = hero.y - hero.dt * dt
+				else
+				   Levels.changeOffset("up", dt * hero.dt)
+				   Entities.y = Entities.y + hero.dt * dt
+				end
+				if Entities.check_collision(hero.x, hero.y) then
+					resolve_collision(hero.dir, Entities.x, Entities.y)
+				end
 			end
 		end
-		if love.keyboard.isDown("left") and get_character_x_pos() > 0 then
-			hero.dir = "left"
-			if hero.x > 150 then
-			   hero.x = hero.x - hero.dt * dt
-			else
-			   Levels.changeOffset("left", dt * hero.dt)
-			   Entities.x = Entities.x + hero.dt * dt
+		if love.keyboard.isDown("right") or love.joystick.getHat(1, 1) == "r" or love.joystick.getHat(1, 1) == "ru" or love.joystick.getHat(1, 1) == "rd" then
+			if get_character_x_pos() < (Levels.map_w - 1) * 40 then
+				hero.dir = "right"
+				if hero.x < 600 then
+				   hero.x = hero.x + hero.dt * dt
+				else
+				   Levels.changeOffset("right", dt * hero.dt)
+				   Entities.x = Entities.x - hero.dt * dt
+				end
+				if Entities.check_collision(hero.x, hero.y) then
+					--hero.x = hero.x - hero.dt * dt
+					resolve_collision(hero.dir, Entities.x, Entities.y)
+				end
 			end
-			if Entities.check_collision(hero.x, hero.y) then
-				--hero.x = hero.x + hero.dt * dt
-				resolve_collision(hero.dir, Entities.x, Entities.y)
+		end
+		if love.keyboard.isDown("left") or love.joystick.getHat(1, 1) == "l" or love.joystick.getHat(1, 1) == "lu" or love.joystick.getHat(1, 1) == "ld" then 
+			if get_character_x_pos() > 0 then
+				hero.dir = "left"
+				if hero.x > 150 then
+				   hero.x = hero.x - hero.dt * dt
+				else
+				   Levels.changeOffset("left", dt * hero.dt)
+				   Entities.x = Entities.x + hero.dt * dt
+				end
+				if Entities.check_collision(hero.x, hero.y) then
+					--hero.x = hero.x + hero.dt * dt
+					resolve_collision(hero.dir, Entities.x, Entities.y)
+				end
 			end
 		end
 		Levels.update()
